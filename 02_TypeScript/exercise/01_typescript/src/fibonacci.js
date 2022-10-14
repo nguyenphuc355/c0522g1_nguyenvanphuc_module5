e6de57e33661a109df480bf1f903b08c18fe9320
nguyenphuc355 <nguyenvanphuca9999@gmail.com>
2022-10-14 14:10:37 +0700

    up

--- 02_TypeScript/exercise/01_typescript/src/fibonacci.js
+++ 02_TypeScript/exercise/01_typescript/src/fibonacci.js
@@ -1,16 +1,30 @@
-function sumFibonacci(amount) {
-    var num1 = 0;
-    var num2 = 1;
-    var num3;
-    var sum = num1 + num2;
-    var arr = num1 + "," + num2;
-    for (var i = 3; i <= amount; i++) {
-        num3 = num1 + num2;
-        sum += num3;
-        arr += "," + num3;
-        num1 = num2;
-        num2 = num3;
+// function sumFibonacci(amount: number): string {
+//     let num1: number = 0;
+//     let num2: number = 1;
+//     let num3: number;
+//     let sum: number = num1 + num2;
+//     let arr: string = num1 + "," + num2;
+//     for (let i = 3; i <= amount; i++) {
+//         num3 = num1 + num2;
+//         sum += num3;
+//         arr += "," + num3;
+//         num1 = num2;
+//         num2 = num3;
+//     }
+//     return "Dãy fibonacci: ( " + arr + " )" + " ,Tổng: " + sum;
+// }
+//
+// console.log(sumFibonacci(10));
+function fibonacci(num) {
+    if (num <= 1) {
+        return num;
     }
-    return "Dãy fibonacci: ( " + arr + " )" + " ,Tổng: " + sum;
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
-console.log(sumFibonacci(10));
+var arr = [];
+var sum = 0;
+for (var i = 0; i < 10; i++) {
+    arr.push(fibonacci(i));
+    sum += fibonacci(i);
+}
+console.log("Dãy fibonacci: " + arr + ", Tổng: " + sum);
--- 02_TypeScript/exercise/01_typescript/src/fibonacci.ts
+++ 02_TypeScript/exercise/01_typescript/src/fibonacci.ts
@@ -1,17 +1,19 @@
-function sumFibonacci(amount: number): string {
-    let num1: number = 0;
-    let num2: number = 1;
-    let num3: number;
-    let sum: number = num1 + num2;
-    let arr: string = num1 + "," + num2;
-    for (let i = 3; i <= amount; i++) {
-        num3 = num1 + num2;
-        sum += num3;
-        arr += "," + num3;
-        num1 = num2;
-        num2 = num3;
+
+
+function fibonacci(num: number): number {
+    if (num <= 1) {
+        return num;
     }
-    return "Dãy fibonacci: ( " + arr + " )" + " ,Tổng: " + sum;
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
-console.log(sumFibonacci(10));
+let arr: Array<number> = [];
+let sum: number = 0;
+
+for (let i = 0; i < 10; i++) {
+    arr.push(fibonacci(i));
+    sum += fibonacci(i);
+}
+
+console.log("Dãy fibonacci: " + arr + ", Tổng: " + sum);
