3d4d8b9962ef7414b54465ebf285eb595eda2966
nguyenphuc355 <nguyenvanphuca9999@gmail.com>
2022-10-21 22:28:57 +0700

    up

--- 00_case_study_module5/case_study_angular/case-study-furama-angular/src/app/app.module.ts
+++ 00_case_study_module5/case_study_angular/case-study-furama-angular/src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { FacilityListComponent } from './facility-list/facility-list.component';
-import { EditFacilityListComponent } from './edit-facility-list/edit-facility-list.component';
-import { CreateFacilityListComponent } from './create-facility-list/create-facility-list.component';
-import { CustomerListComponent } from './customer-list/customer-list.component';
-import { EditCustomerComponent } from './edit-customer/edit-customer.component';
-import { CreateCustomerComponent } from './create-customer/create-customer.component';
-import { ContratListComponent } from './contrat-list/contrat-list.component';
-import { CreateContractComponent } from './create-contract/create-contract.component';
+import { FacilityListComponent } from './facility/facility-list/facility-list.component';
+import { EditFacilityListComponent } from './facility/edit-facility-list/edit-facility-list.component';
+import { CreateFacilityListComponent } from './facility/create-facility-list/create-facility-list.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+import { EditCustomerComponent } from './customer/edit-customer/edit-customer.component';
+import { CreateCustomerComponent } from './customer/create-customer/create-customer.component';
+import { ContratListComponent } from './contract/contrat-list/contrat-list.component';
+import { CreateContractComponent } from './contract/create-contract/create-contract.component';
 
 @NgModule({
   declarations: [
---
+++ 00_case_study_module5/case_study_angular/case-study-furama-angular/src/app/model/facility.ts
@@ -0,0 +1,14 @@
+export interface Facility {
+  id: number;
+  name: string;
+  area: number;
+  cost: string;
+  maxPeople: number;
+  standardRoom: string;
+  descriptionOtherConvenience: string;
+  poolArea: number;
+  numberOfFloors: number;
+  facilityFree: string;
+  facilityType: string;
+  renType: string;
+}
